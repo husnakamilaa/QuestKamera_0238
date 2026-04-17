c284e78b357fabc95e3afbe09e2dd440546c5726
husnakamilaa <husna.kamila.ft23@mail.umy.ac.id>
2026-04-17 09:57:35 +0700

    feat: inisialisasi event camera dan turunannya

---
+++ lib/bloc/camera_event.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+sealed class CameraEvent{}
+
+final class InitialCamera extends CameraEvent{}
+final class SwitchCamera extends CameraEvent{}
+final class ToggleFlash extends CameraEvent{}
+final class TakePicture extends CameraEvent{
+  final void Function(File imageFile) onPictureTaken;
+  TakePicture(this.onPictureTaken);
+}
+final class TapToFocus extends CameraEvent{
+  final Offset position;
+  final Size previewSize;
+  TapToFocus(this.position, this.previewSize);
+}
+final class PickImageFromGallery extends CameraEvent{}
+final class OpenCameraAndCapture extends CameraEvent{
+  final BuildContext context;
+  OpenCameraAndCapture(this.context);
+}
+final class DeleteImage extends CameraEvent{}
+final class ClearSnackbar extends CameraEvent{}
+final class RequestPermission extends CameraEvent{}
+
